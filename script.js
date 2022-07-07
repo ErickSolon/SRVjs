38409924920046a77b1ce1b2bb7cf13adb7f660f
Érick S <erick.solon.prog@gmail.com>
2022-07-07 12:31:57 -0300

    .

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-const form = document.forms.simulacao;
+const form = document.forms.Simulacao;
+
+
 
 form.addEventListener("submit", (e) => {
+
   e.preventDefault();
+
+
 
   const { veiculo, ano, preco } = form;
 
-  const infoVeiculo = document.querySelector("#infoVeiculo");
-  const valEntrada = document.querySelector("#valEntrada");
-  const valParcela = document.querySelector("#valParcela");
+
+
+  const infoVeiculo = document.querySelector("#veiculo");
 
   const anoVeiculo = new Date(ano.value).getFullYear();
-  
-  const entrada = +veiculo.value * (50 / 100);
-  const parcela = (+preco.value * (50 / 100)) / 48;
 
-  const intlEntrada = Intl.NumberFormat("pt-br", {
-    style: "currency",
-    currency: "BRL",
-    maximumSignificantDigits: 2,
-  }).format(entrada);
-  const intlParcela = Intl.NumberFormat("pt-br", {
-    style: "currency",
-    currency: "BRL",
-    maximumSignificantDigits: 2,
-  }).format(parcela);
+
+
+const entrada = 15000
+
+const valorTotalPosEntrada = +preco.value-entrada
+
+
+
+
+const valorTotalPraParcela = valorTotalPosEntrada
+
+
+
+const parcela = valorTotalPraParcela/48
+
+ 
+
+  //const parcela = (preco.value - 15000 * (50 / 100)) / 48 + 520.848;
+
+
 
   infoVeiculo.textContent = `${veiculo.value} de ano ${anoVeiculo}`;
-  valEntrada.textContent = `Entrada de: ${intlEntrada}`;
-  valParcela.textContent = ` Parcelas: 48x de ${intlParcela}`;
+
+  valParcela.textContent = ` Parcelas: 48x de ${parcela}`;
+
 });
